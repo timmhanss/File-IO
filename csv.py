621976f7199810ca2807ad972aafeeed24394415
timmhanss <21timothy.hansen@gmail.com>
2022-11-25 13:10:32 +0700

    Added csv practice. sorted() is a bitch

--- csv.py
+++ csv.py
@@ -7,6 +7,17 @@ def csvRead():
         for line in f:
             # row = line.strip().split(",") # The file has 3 variables - method 1
             food, taste, color = line.strip().split(",") # If it has 3 vars, why not just call it directly?
-            print(f"{food} tastes{taste} and looks{color}.", sep="") # Remove all trailing space in the line. It adds spaces first somehow.
+            #print(f"{food} tastes{taste} and looks{color}.", sep="") # Remove all trailing space in the line. It adds spaces first somehow.
             
-main()
+def csvReadDict():
+    basket = []
+    with open ("csv-file.txt") as f:
+        for line in f:
+            food, taste, color = line.strip().split(",")
+            cart = {"Food": food, "Taste" : taste, "Color" : color}
+            basket.append(cart)
+            
+    for cart in sorted(basket, key=lambda x: x["Food"], reverse=True): # When modifying dicts, sorted() needs a comparator. Lists doesn't need a comparator to work
+        print(f"{cart['Food']} tastes{cart['Taste']} and looks{cart['Color']}.") # why the fuck did i add a colon - ex: {cart:['Food']}
+#main()
+csvReadDict()
